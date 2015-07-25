0d05069bb6d527df8dae358cfc182213583095f6
NonWhite <wperezurcia@gmail.com>
2015-07-25 20:05:03 -0300

    change number of lines to read per dataset

--- scripts/script.py
+++ scripts/script.py
@@ -2,12 +2,13 @@ import os
 from subprocess import call
 
 DATASET_FILE = 'sets.txt'
-PROGRAM = 'pypy ../code/builder.py %s %s %s %s'
+PROGRAM = 'pypy ../code/builder.py %s %s %s'
+CONF_LINES = 4
 
 if __name__ == "__main__" :
 	with open( DATASET_FILE , 'r' ) as f :
 		lines = [ l[ :-1 ] for l in f.readlines() ]
-		for i in xrange( 0 , len( lines ) , 5 ) :
-			train , test , ommit , out , _ = lines[ i:(i+5) ]
-			inst = ( PROGRAM % ( train , test , ommit , out ) ).split()
+		for i in xrange( 0 , len( lines ) , CONF_LINES ) :
+			dataset , ommit , out , _ = lines[ i:(i+CONF_LINES) ]
+			inst = ( PROGRAM % ( dataset , ommit , out ) ).split()
 			call( inst )
