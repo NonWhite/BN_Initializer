a10ec5def212ecb76391e5249155df943963c470
NonWhite <wperezurcia@gmail.com>
2015-08-05 19:58:03 -0300

    added statistics (stdev and mean) for number of iterations and best score

--- scripts/plot.py
+++ scripts/plot.py
@@ -1,25 +1,25 @@
 import os
+import statistics
 import numpy as np
 from pylab import *
 from os.path import *
 from copy import deepcopy as copy
 
 color = [ 'b' , 'r' , 'g' ]
+#types = [ 'random' , 'unweighted' , 'weighted' ]
+#datasets = [ 'alarm' , 'census' , 'epigenetics' , 'image' , 'letter' , 'lungCancer' , 'mushroom' , 'sensors' , 'spectf' , 'steelPlates' ]
+datasets = [ 'census' , 'letter' , 'image' , 'mushroom' ]
 types = [ 'random' , 'unweighted' , 'weighted' ]
-datasets = [ 'alarm' , 'census' , 'epigenetics' , 'image' , 'letter' , 'mushroom' , 'sensors' , 'steelPlates' ]
 SOL_DELIMITER = ' =='
 IMAGES_DIR = '../doc/images/'
 
 def read_content( fpath , name ) :
-	data = { 'name' : name , 'solutions' : [] , 'init_time' : 0 }
-	sol = { 'score' : [] , 'iterations' : 0 , 'total_time' : 0 }
+	data = { 'name' : name , 'solutions' : [] }
+	sol = { 'score' : [] , 'iterations' : 0 }
 	print fpath
 	with open( fpath , 'r' ) as f :
 		lines = [ l[ :-1 ] for l in f.readlines() ]
-		idx = 1
-		if lines[ 0 ].startswith( 'TIME(init' ) :
-			init_time = float( lines[ 0 ].split( ' = ' )[ -1 ] )
-			data[ 'init_time' ] = init_time
+		idx = 0
 		while idx < len( lines ) :
 			if lines[ idx ].startswith( SOL_DELIMITER ) :
 				solution = copy( sol )
@@ -31,40 +31,45 @@ def read_content( fpath , name ) :
 					elif line.startswith( 'NUM IT' ) :
 						iterations = int( line.split( ' = ' )[ -1 ] )
 						solution[ 'iterations' ] = iterations
-					elif line.startswith( 'TIME(total' ) :
-						total_time = float( line.split( ' = ' )[ -1 ] )
-						solution[ 'total_time' ] = total_time
 					idx += 1
 					if idx >= len( lines ) or \
 						lines[ idx ].startswith( SOL_DELIMITER ) or \
 						lines[ idx ].startswith( 'BEST' ) :
 						break
 				data[ 'solutions' ].append( solution )
-			if lines[ idx ].startswith( 'BEST' ) : break
-	max_val = max( s[ 'score' ][ -1 ] for s in data[ 'solutions' ] if len( s[ 'score' ] ) > 0 )
-	for s in data[ 'solutions' ] :
-		if len( s[ 'score' ] ) == 0 :
-			s[ 'score' ].append( 0.0 )
-	num_sols = sum( [ 1 for s in data[ 'solutions' ] if s[ 'score' ][ -1 ] == max_val ] )
+			if idx >= len( lines ) or lines[ idx ].startswith( 'BEST' ) : break
+	best_sc = [ s[ 'score' ][ -1 ] for s in data[ 'solutions' ] ]
+	max_sc = max( best_sc )
+	stdev_sc = statistics.stdev( best_sc )
+	num_sols = sum( [ 1 for s in data[ 'solutions' ] if s[ 'score' ][ -1 ] == max_sc ] )
 	total_sols = len( data[ 'solutions' ] )
-	max_iterations = max( [ s[ 'iterations' ] for s in data[ 'solutions' ] ] )
-	avg_iterations = float( sum( [ s[ 'iterations' ] for s in data[ 'solutions' ] ] ) ) / total_sols
-	max_total_time = max( [ s[ 'total_time' ] for s in data[ 'solutions' ] ] )
-	avg_total_time = sum( [ s[ 'total_time' ] for s in data[ 'solutions' ] ] ) / total_sols
+	perc_sols = float( num_sols ) / total_sols * 100.0
+	all_it = [ s[ 'iterations' ] for s in data[ 'solutions' ] ]
+	max_iterations = max( all_it )
+	avg_iterations = statistics.mean( all_it )
+	stdev_iterations = statistics.stdev( all_it )
 	print name.upper()
-	#print "INITIALIZATION TIME = %s" % data[ 'init_time' ]
-	#print "MAX TOTAL TIME = %s" % max_total_time
-	#print "AVG TOTAL TIME = %s" % avg_total_time
-	print "MAX SCORE = %s" % max_val
+	print "MAX SCORE = %s" % max_sc
+	print "STDEV MAX SCORE = %s" % stdev_sc
 	print "TOTAL NUM SOLUTIONS = %s" % total_sols
 	print "NUM OF SOLUTIONS WITH MAX SCORE = %s" % num_sols
-	print "PERCENTAGE WITH MAX SCORE = %s" % ( float( num_sols ) / total_sols )
+	print "PERCENTAGE WITH MAX SCORE = %s" % perc_sols
 	print "MAX NUM ITERATIONS = %s" % max_iterations
 	print "AVG NUM ITERATIONS = %s" % avg_iterations
-	best_sols = [ s for s in data[ 'solutions' ] if s[ 'score' ][ -1 ] == max_val ]
-	sorted( best_sols , key = lambda s : s[ 'iterations' ] , reverse = True )
-	data[ 'score' ] = best_sols[ 0 ][ 'score' ]
-	data[ 'iterations' ] = best_sols[ 0 ][ 'iterations' ]
+	print "STDEV NUM ITERATIONS = %s" % stdev_iterations
+	print "%.3f +/- %.2f & %.2f & %.2f +/- %.2f" % ( max_sc , stdev_sc , perc_sols , avg_iterations , stdev_iterations )
+	max_length = max( [ len( s[ 'score' ] ) for s in data[ 'solutions' ] ] )
+	avg_scores = []
+	for i in xrange( max_length ) :
+		sc = 0.0
+		q = 0
+		for sol in data[ 'solutions' ] :
+			if len( sol[ 'score' ] ) <= i : continue
+			q += 1
+			sc += sol[ 'score' ][ i ]
+		avg_scores.append( sc / q )
+	data[ 'score' ] = avg_scores
+	data[ 'iterations' ] = len( avg_scores )
 	data.pop( 'solutions' )
 	return data
 
@@ -81,23 +86,17 @@ def makePlot( directory , dataname ) :
 		f = "%s%s_%s.txt" % ( directory , dataname , t )
 		networkdata.append( read_content( f , t ) )
 	max_iterations = max( [ d[ 'iterations' ] for d in networkdata ] )
-	for d in networkdata :
-		missing = max_iterations - len( d[ 'score' ] )
-		best = d[ 'score' ][ -1 ]
-		for r in xrange( missing ) :
-			d[ 'score' ].append( best )
 	for i in range( len( networkdata ) ) :
 		data = networkdata[ i ]
 		y = data[ 'score' ]
 		x = range( 1 , len( y ) + 1 )
 		col = color[ i ]
 		addCurve( x , y , col , data[ 'name' ] )
-		#addPoint( data[ 'iterations' ] , data[ 'score' ][ -1 ] , col )
 	legend( loc = 'lower right' )
 	xlabel( 'Iteration' )
 	ylabel( 'BIC Score' )
-	savefig( "%s%s" % ( IMAGES_DIR , dataname ) )
-	#show()
+	#savefig( "%s%s" % ( IMAGES_DIR , dataname ) )
+	show()
 	clf()
 	print ' ====================================================== '
 
