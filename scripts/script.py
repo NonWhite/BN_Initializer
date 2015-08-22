5e276af6f7f9014862f8cac69eb73cc5773d9465
NonWhite <wperezurcia@gmail.com>
2015-08-22 07:08:26 -0300

    change from 4 to 3 lines per dataset

--- scripts/script.py
+++ scripts/script.py
@@ -2,13 +2,14 @@ import os
 from subprocess import call
 
 DATASET_FILE = 'sets.txt'
-PROGRAM = 'pypy ../code/builder.py %s %s %s'
-CONF_LINES = 4
+PROGRAM = 'pypy ../code/builder.py %s %s'
+CONF_LINES = 3
 
 if __name__ == "__main__" :
 	with open( DATASET_FILE , 'r' ) as f :
 		lines = [ l[ :-1 ] for l in f.readlines() ]
 		for i in xrange( 0 , len( lines ) , CONF_LINES ) :
-			dataset , ommit , out , _ = lines[ i:(i+CONF_LINES) ]
-			inst = ( PROGRAM % ( dataset , ommit , out ) ).split()
+			dataset , ommit , _ = lines[ i:(i+CONF_LINES) ]
+			print "PROCESSING DATASET %s" % dataset
+			inst = ( PROGRAM % ( dataset , ommit ) ).split()
 			call( inst )
